9ea0b5335ca04ba09431827283b5ddacf205521c
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-11 13:05:18 +0300

    Signup and Login forms added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 import Mantra from './components/Mantra';
 import Meditation from './components/Meditation';
 import Mindfulness from './components/Mindfulness';
 import Movement from './components/Movement';
 import Navbar from './components/Navbar/Navbar';
+// import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <Router>
+    
       <Navbar/>
+     <Login/>
           <Routes>
 
           <Route path="/" element={<Meditation/>}/>
